e6b9839690364eb603df1fbf8007ae328f277e0c
Murilo Benatti <benatti1104@gmail.com>
2023-08-24 23:49:13 -0300

    tests

--- Js/script.js
+++ Js/script.js
@@ -1,18 +1,84 @@
 class Produto {
     constructor() {
 
-        this.id = 0;
-        this.nomeProduto ='';
-        this.valor = 0;
+        this.id = 1;
+        this.arrayVagas = [];
+
+    }
+
+    salvar() {
+        let produto = this.lerDados();
+
+        if (this.validaCampos(produto)) {
+
+            this.adicionar(produto);
+ 
+        }
+       this.listaTabela();
+    }
+
+    listaTabela(){
+        let tbody = document.getElementById('tbody');
+
+        for(let i = 0; i < this.arrayVagas.length; i++) {
+            let tr = tbody.insertRow();
+            
+            let td_id = tr.insertCell();
+            let td_name = tr.insertCell();
+            let td_vagas = tr.insertCell();
+   
+            
+            td_id.innerText = this.arrayVagas[i].id;
+            td_name.innerText = this.arrayVagas[i].nome;
+            td_vagas.innerText = this.arrayVagas[i].vagas;
+         
+
+
+        }
+    }
+
+
+    adicionar(produto) {
+        this.arrayVagas.push(produto);
+        this.id ++;
 
 
     }
 
-    adicionar() {
-        alert('Vamos Adicionar o produto');
+    lerDados() {
+        let produto = {}
+
+        produto.id = this.id;
+        produto.nomeProduto = document.getElementById('nome').value;
+        produto.vagas = document.getElementById('vagas').value;
+
+        return nome;
+
     }
 
-    excluir() {
+    validaCampos(produto) {
+
+        let msg = '';
+
+        if (produto.nomeProduto == '') {
+            msg += '- Informe o nome do hospede \n'
+        }
+
+
+        if (produto.vagas == '') {
+            msg += '- Informe a vaga'
+        }
+
+        if (msg != '') {
+            alert(msg);
+            return false
+        }
+
+        return true;
+
+    }
+
+    cancelar() {
         alert('item deletado');
     }
 }
