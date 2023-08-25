6a51d03b18da80bb9dd27b0dfa47c043d4977fe6
Murilo Benatti <benatti1104@gmail.com>
2023-08-24 23:01:10 -0300

    Easy fix

---
+++ Js/script.js
@@ -0,0 +1,21 @@
+class Produto {
+    constructor() {
+
+        this.id = 0;
+        this.nomeProduto ='';
+        this.valor = 0;
+
+
+    }
+
+    adicionar() {
+        alert('Vamos Adicionar o produto');
+    }
+
+    excluir() {
+        alert('item deletado');
+    }
+}
+
+
+var produto = new Produto();
